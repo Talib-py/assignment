7e84f0617d29847a466be6122825e86eaa30ea34
Talib Shaikh <155802639+Talib-py@users.noreply.github.com>
2025-02-26 02:25:16 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # FastAPI Backend URL
-API_URL = "http://127.0.0.1:8000/chat/"
+API_URL = "https://assignment-499w.onrender.com//chat/"
 
 st.title("Titanic AI Chatbot")
 
